7904093d67803f9a98bac3339379ed7dccf2cb50
Gokul Krishna <53399680+gokursx@users.noreply.github.com>
2024-05-08 18:50:40 -0700

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+ const horizontalJoin = letters.map(ls => ls.join(''));
 
-module.exports = wordSearch
+    let horizontal = letters[0].length;
+    let vertical = letters.length;
+    //Looping through the array to check whether letter is present in word
+    //Returns true if present and false if not
+    for (let i = 0; i < horizontal; i++) {
+        let temp = [];
+        for (let j = 0; j < vertical; j++) {
+            temp.push(letters[j][i]);
+            if (temp.includes(word)) return true;
+        }
+
+    }
+    
+    //Looping through array
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    return false;
+};
+
+module.exports = wordSearch;
